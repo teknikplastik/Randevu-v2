7e98825303ea1e7d0ed95dd195f8d6093d6bf8e5
Teknik Plastik <130987171+teknikplastik@users.noreply.github.com>
2025-06-14 10:45:52 +0300

    Add files via upload

---
+++ app/src/main/java/com/drerensagiroglu/randevu/MainActivity.java
@@ -0,0 +1,5 @@
+package com.drerensagiroglu.randevu;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor-cordova-android-plugins/src/main/java/com/getcapacitor/cordova/android/plugins/CordovaPlugins.java
@@ -0,0 +1,5 @@
+package com.getcapacitor.cordova.android.plugins;
+
+public class CordovaPlugins {
+    // Empty class for Capacitor Cordova plugins
+}
